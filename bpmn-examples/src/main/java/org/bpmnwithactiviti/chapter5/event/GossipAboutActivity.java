89f6da75e7945d3a294eef82dfa87fbcaec6c62e
tijs.rademakers <tijs.rademakers@a29e19ff-66de-9391-0f37-b916ae5d8a96>
2011-01-11 21:43:39 +0000

    fixed gossip examples

--- bpmn-examples/src/main/java/org/bpmnwithactiviti/chapter5/event/GossipAboutActivity.java
+++ bpmn-examples/src/main/java/org/bpmnwithactiviti/chapter5/event/GossipAboutActivity.java
@@ -1,10 +1,9 @@
 package org.bpmnwithactiviti.chapter5.event;
 
 import org.activiti.engine.delegate.DelegateExecution;
-import org.activiti.engine.delegate.JavaDelegate;
 import org.activiti.engine.impl.pvm.delegate.ExecutionListenerExecution;
 
-public class GossipAboutActivity implements JavaDelegate {
+public class GossipAboutActivity {
 	
 	public void gossipStart(ExecutionListenerExecution execution) {
 		System.out.println("Oh my the following event took place = " + execution.getEventName());
@@ -14,12 +13,6 @@ public class GossipAboutActivity implements JavaDelegate {
 	public void gossipEnd(ExecutionListenerExecution execution) {
 		System.out.println("I can gossip about process variables and execution id = " + execution.getId());
 		EventUtil.addEvent(execution, "activity");
-	}
-
-	@Override
-	public void execute(DelegateExecution arg0) throws Exception {
-		// TODO Auto-generated method stub
-		
 	}
 
 }
