5cf381dc0a0568cecec0710fd371d8f549a4f15b
tijs.rademakers <tijs.rademakers@a29e19ff-66de-9391-0f37-b916ae5d8a96>
2011-04-16 14:59:34 +0000

    initial commit

---
+++ book-sales-webapp/src/main/java/org/bpmnwithactiviti/chapter6/data/model/SalesOpportunity.java
@@ -0,0 +1,54 @@
+package org.bpmnwithactiviti.chapter6.data.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+
+/**
+ * @author Tijs Rademakers
+ */
+
+@Entity
+public class SalesOpportunity {
+  
+  @Id
+  @GeneratedValue
+  private long opportunityId;
+  
+  private String product;
+  private int expectedQuantity;
+  private String description;
+  
+  public long getOpportunityId() {
+    return opportunityId;
+  }
+  
+  public void setOpportunityId(long opportunityId) {
+    this.opportunityId = opportunityId;
+  }
+  
+  public String getProduct() {
+    return product;
+  }
+  
+  public void setProduct(String product) {
+    this.product = product;
+  }
+  
+  public int getExpectedQuantity() {
+    return expectedQuantity;
+  }
+  
+  public void setExpectedQuantity(int expectedQuantity) {
+    this.expectedQuantity = expectedQuantity;
+  }
+  
+  public String getDescription() {
+    return description;
+  }
+  
+  public void setDescription(String description) {
+    this.description = description;
+  }
+}
