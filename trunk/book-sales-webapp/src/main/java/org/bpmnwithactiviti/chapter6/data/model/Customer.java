c35e0b28d0b134d25441b076894eb7d870203d27
tijs.rademakers <tijs.rademakers@a29e19ff-66de-9391-0f37-b916ae5d8a96>
2011-04-16 14:59:34 +0000

    initial commit

---
+++ trunk/book-sales-webapp/src/main/java/org/bpmnwithactiviti/chapter6/data/model/Customer.java
@@ -0,0 +1,70 @@
+package org.bpmnwithactiviti.chapter6.data.model;
+
+import java.util.List;
+
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+
+/**
+ * @author Tijs Rademakers
+ */
+
+@Entity
+public class Customer {
+  
+  @Id
+  @GeneratedValue
+  private long customerId;
+  
+  private String customerName;
+  private String customerAddress;
+  private String contactPerson;
+  
+  @ElementCollection(targetClass=org.bpmnwithactiviti.chapter6.data.model.SalesOpportunity.class, fetch=FetchType.EAGER)
+  private List<SalesOpportunity> saleOpportunities;
+
+  public long getCustomerId() {
+    return customerId;
+  }
+  
+  public void setCustomerId(long customerId) {
+    this.customerId = customerId;
+  }
+  
+  public String getCustomerName() {
+    return customerName;
+  }
+  
+  public void setCustomerName(String customerName) {
+    this.customerName = customerName;
+  }
+  
+  public String getCustomerAddress() {
+    return customerAddress;
+  }
+  
+  public void setCustomerAddress(String customerAddress) {
+    this.customerAddress = customerAddress;
+  }
+  
+  public String getContactPerson() {
+    return contactPerson;
+  }
+ 
+  public void setContactPerson(String contactPerson) {
+    this.contactPerson = contactPerson;
+  }
+
+  public List<SalesOpportunity> getSaleOpportunities() {
+    return saleOpportunities;
+  }
+
+  public void setSaleOpportunities(List<SalesOpportunity> saleOpportunities) {
+    this.saleOpportunities = saleOpportunities;
+  }
+
+}
