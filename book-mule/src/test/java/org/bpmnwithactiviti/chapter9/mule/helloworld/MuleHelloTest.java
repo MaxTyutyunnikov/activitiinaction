5602b83b5b9b500e1f515c632eca542f88ab29f7
tijs.rademakers <tijs.rademakers@a29e19ff-66de-9391-0f37-b916ae5d8a96>
2011-05-17 07:59:17 +0000

    Added Mule examples for chapter 9

--- book-mule/src/test/java/org/bpmnwithactiviti/chapter9/mule/helloworld/MuleHelloTest.java
+++ book-mule/src/test/java/org/bpmnwithactiviti/chapter9/mule/helloworld/MuleHelloTest.java
@@ -1,8 +1,10 @@
-package org.bpmnwithactiviti.chapter9.mule;
+package org.bpmnwithactiviti.chapter9.mule.helloworld;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.fail;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import org.activiti.engine.RuntimeService;
 import org.activiti.engine.runtime.ProcessInstance;
@@ -11,18 +13,19 @@ import org.junit.Test;
 import org.mule.api.MuleContext;
 import org.mule.context.DefaultMuleContextFactory;
 
-public class BookOrderMuleTest extends AbstractTest {
+public class MuleHelloTest extends AbstractTest {
 
   @Test
   public void testSend() throws Exception {
-    MuleContext muleContext = new DefaultMuleContextFactory().createMuleContext("application-context.xml");
+    MuleContext muleContext = new DefaultMuleContextFactory().createMuleContext("helloworld/application-context.xml");
     muleContext.start();
     RuntimeService runtimeService = (RuntimeService) muleContext.getRegistry().get("runtimeService");
-    ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("muleProcess");
+    Map<String, Object> variableMap = new HashMap<String, Object>();
+    variableMap.put("var1", "hello");
+    ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("helloWorldMule", variableMap);
     assertFalse(processInstance.isEnded());
-    Object result = runtimeService.getVariable(processInstance.getProcessInstanceId(), "theVariable");
-    assertEquals(10, result);
-    
+    Object result = runtimeService.getVariable(processInstance.getProcessInstanceId(), "var2");
+    assertEquals("world", result);
     muleContext.stop();
     muleContext.dispose();
   }
