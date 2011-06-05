ac41dff1ae364244ef9584c86c2d5090248cfabf
tijs.rademakers <tijs.rademakers@a29e19ff-66de-9391-0f37-b916ae5d8a96>
2011-06-05 20:03:15 +0000

    Fixed older examples

--- bpmn-examples/src/test/java/org/bpmnwithactiviti/chapter4/LoanRequestTest.java
+++ bpmn-examples/src/test/java/org/bpmnwithactiviti/chapter4/LoanRequestTest.java
@@ -7,132 +7,42 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.mail.internet.MimeMessage;
-
-import org.activiti.engine.runtime.ProcessInstance;
-import org.activiti.engine.task.Task;
+import org.activiti.engine.history.HistoricDetail;
+import org.activiti.engine.history.HistoricVariableUpdate;
 import org.activiti.engine.test.ActivitiRule;
 import org.activiti.engine.test.Deployment;
-import org.junit.After;
-import org.junit.Before;
+import org.bpmnwithactiviti.common.AbstractTest;
 import org.junit.Rule;
 import org.junit.Test;
-import org.subethamail.wiser.Wiser;
-import org.subethamail.wiser.WiserMessage;
 
-public class LoanRequestTest {	
-	@Rule 
-	public ActivitiRule activitiRule = new ActivitiRule("activiti.cfg-mem-mail.xml");
+public class LoanRequestTest extends AbstractTest {	
 	
-	private Wiser wiser;
-	
-	@Before
-	public void startMailServer()
-	{
-		wiser = new Wiser();
-	    wiser.setPort(1025);
-	    wiser.start();
-	}
-	
-	@After
-	public void stopMailServer()
-	{
-		wiser.stop();
-	}
+  @Rule 
+	public ActivitiRule activitiRule = new ActivitiRule("activiti.cfg-mem-fullhistory.xml");
 
 	@Test
-	@Deployment(resources={"chapter4/loanrequest.bpmn20.xml","chapter4/loanRequest.form","chapter4/handleLoanRequest.form"})
-	public void creditCheckTrue() throws Exception {
-		/* The following should work but ... does not
-		FormService formservice = activitiRule.getProcessEngine().getFormService();
-	    Map<String,String> formProperties = new HashMap<String,String>();
-	    formProperties.put("name", "Miss Piggy");
-	    formProperties.put("emailAddress","miss.piggy@localhost");
-	    formProperties.put("income","100");
-	    formProperties.put("loanAmount","10");
-	    ProcessInstance pi = formservice.submitStartFormData("loanrequest:1", formProperties);
-	    */
-		Map<String,Object> processVariables = new HashMap<String,Object> ();
+	@Deployment(resources={"chapter4/loanrequest_firstpart.bpmn20.xml"})
+	public void creditCheckTrue() {
+		Map<String, Object> processVariables = new HashMap<String, Object>();
 		processVariables.put("name", "Miss Piggy");
-		processVariables.put("emailAddress", "miss.piggy@localhost");
 		processVariables.put("income", new Integer(100));
 		processVariables.put("loanAmount", new Integer(10));
-		ProcessInstance pi = activitiRule.getRuntimeService().startProcessInstanceByKey("loanrequest",processVariables);
-	    processVariables = activitiRule.getRuntimeService().getVariables(pi.getId());
-		LoanApplication la = (LoanApplication) processVariables.get("loanApplication");
-		assertEquals(true, la.isCreditCheckOk());
-		List<Task> tasks = activitiRule.getTaskService().createTaskQuery().taskAssignee("fozzie").list();
-	    assertEquals(1, tasks.size());
-	    Task task = tasks.get(0);
-	    assertNotNull(activitiRule.getFormService().getRenderedTaskForm(task.getId()));
-	    Map<String,String> formProperties = new HashMap<String,String>();
-	    formProperties.put("requestApproved","true");
-	    activitiRule.getFormService().submitTaskFormData(task.getId(), formProperties);
-	    assertEquals(0, wiser.getMessages().size());
-	}
-	
-	@Test
-	@Deployment(resources={"chapter4/loanrequest.bpmn20.xml","chapter4/loanRequest.form","chapter4/handleLoanRequest.form"})
-	public void creditCheckFalse() throws Exception {
-
-		/* The following should work but ... does not
-		FormService formservice = activitiRule.getProcessEngine().getFormService();
-	    Map<String,String> formProperties = new HashMap<String,String>();
-	    formProperties.put("name", "Miss Piggy");
-	    formProperties.put("emailAddress","miss.piggy@localhost");
-	    formProperties.put("income","10");
-	    formProperties.put("loanAmount","100");
-	    ProcessInstance pi = formservice.submitStartFormData("loanrequest:1", formProperties);
-	    */
-		Map<String,Object> processVariables = new HashMap<String,Object> ();
-		processVariables.put("name", "Miss Piggy");
 		processVariables.put("emailAddress", "miss.piggy@localhost");
-		processVariables.put("income", new Integer(10));
-		processVariables.put("loanAmount", new Integer(100));
-		ProcessInstance pi = activitiRule.getRuntimeService().startProcessInstanceByKey("loanrequest",processVariables);
-	    processVariables = activitiRule.getRuntimeService().getVariables(pi.getId());
-		LoanApplication la = (LoanApplication) processVariables.get("loanApplication");
-		assertEquals(false, la.isCreditCheckOk());
-		List<Task> tasks = activitiRule.getTaskService().createTaskQuery().taskAssignee("fozzie").list();
-	    assertEquals(1, tasks.size());
-	    Task task = tasks.get(0);
-	    assertNotNull(activitiRule.getFormService().getRenderedTaskForm(task.getId()));
-	    Map<String,String> formProperties = new HashMap<String,String>();
-	    formProperties.put("requestApproved","false");
-	    formProperties.put("motivation","Motivation for the rejection");
-	    activitiRule.getFormService().submitTaskFormData(task.getId(), formProperties);
-	    // activitiRule.getTaskService().complete(task.getId());
-		List<WiserMessage> messages = wiser.getMessages();
-	    assertEquals(1, messages.size());
-	    WiserMessage message = messages.get(0);
-	    MimeMessage mimeMessage = message.getMimeMessage();
-	    assertEquals("Loan Request Denied", mimeMessage.getHeader("Subject", null));
-	}
-	
-	/*
-	 * The next test takes 80 seconds to complete. Therefore it is disabled by default.
-	 * In order to enable the test remove the comments before the @Test annotation.
-	 */
-	// @Test
-	@Deployment(resources={"chapter4/loanrequest.bpmn20.xml","chapter4/loanRequest.form","chapter4/handleLoanRequest.form"})
-	public void testEscalation() throws Exception {
-		Map<String,Object> processVariables = new HashMap<String,Object> ();
-		processVariables.put("name", "Miss Piggy");
-		processVariables.put("emailAddress", "miss.piggy@localhost");
-		processVariables.put("income", new Integer(100));
-		processVariables.put("loanAmount", new Integer(10));
-		activitiRule.getRuntimeService().startProcessInstanceByKey("loanrequest",processVariables);
-		System.out.println("Testing escalation ... please wait for 80 seconds.");
-	    assertEquals(1, activitiRule.getTaskService().createTaskQuery().taskAssignee("fozzie").list().size());
-		Thread.sleep(80000);
-	    assertEquals(0, activitiRule.getTaskService().createTaskQuery().taskAssignee("fozzie").list().size());
-	    List<Task> tasks = activitiRule.getTaskService().createTaskQuery().taskCandidateGroup("management").list();
-	    assertEquals(1, tasks.size());
-	    Task task = tasks.get(0);
-	    assertNotNull(activitiRule.getFormService().getRenderedTaskForm(task.getId()));
-	    Map<String,String> formProperties = new HashMap<String,String>();
-	    formProperties.put("requestApproved","true");
-	    activitiRule.getFormService().submitTaskFormData(task.getId(), formProperties);
-	    assertEquals(0, wiser.getMessages().size());
+		activitiRule.getRuntimeService().startProcessInstanceByKey(
+		        "loanrequest", processVariables);
+		
+		List<HistoricDetail> historyVariables = activitiRule.getHistoryService()
+		    .createHistoricDetailQuery()
+		    .variableUpdates()
+		    .orderByTime()
+		    .desc()
+		    .list();
+		
+		assertNotNull(historyVariables);
+		assertEquals(7, historyVariables.size());
+		HistoricVariableUpdate loanAppUpdate = ((HistoricVariableUpdate) historyVariables.get(0));
+		assertEquals("loanApplication", loanAppUpdate.getVariableName());
+		LoanApplication la = (LoanApplication) loanAppUpdate.getValue();
+		assertEquals(true, la.isCreditCheckOk()); 
 	}
 }
