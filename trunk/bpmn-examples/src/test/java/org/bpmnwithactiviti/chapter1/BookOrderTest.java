899a3d789fc589eacdbbb4578fb9bce8b9b733eb
tijs.rademakers@gmail.com <tijs.rademakers@gmail.com@a29e19ff-66de-9391-0f37-b916ae5d8a96>
2011-08-07 19:38:12 +0000

    Added chapter 1 pictures

--- trunk/bpmn-examples/src/test/java/org/bpmnwithactiviti/chapter1/BookOrderTest.java
+++ trunk/bpmn-examples/src/test/java/org/bpmnwithactiviti/chapter1/BookOrderTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertNotNull;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.activiti.engine.IdentityService;
 import org.activiti.engine.ProcessEngine;
 import org.activiti.engine.ProcessEngineConfiguration;
 import org.activiti.engine.RepositoryService;
@@ -21,9 +22,10 @@ public class BookOrderTest {
 		ProcessEngine processEngine = ProcessEngineConfiguration
 			.createStandaloneProcessEngineConfiguration()
 		 	.buildProcessEngine(); 
-		 
-		RuntimeService runtimeService = processEngine.getRuntimeService(); 
+		
 		RepositoryService repositoryService = processEngine.getRepositoryService();
+		RuntimeService runtimeService = processEngine.getRuntimeService();
+		IdentityService identityService = processEngine.getIdentityService();
 		TaskService taskService = processEngine.getTaskService(); 
 		repositoryService.createDeployment()
 			.addClasspathResource("chapter1/bookorder.bpmn20.xml")
@@ -31,11 +33,10 @@ public class BookOrderTest {
 		
 		Map<String, Object> variableMap = new HashMap<String, Object>();
 		variableMap.put("isbn", "123456");
+		identityService.setAuthenticatedUserId("kermit");
 		ProcessInstance processInstance = runtimeService.startProcessInstanceByKey(
 				"bookorder", variableMap);
 		assertNotNull(processInstance.getId());
-		System.out.println("id " + processInstance.getId() + " " 
-				+ processInstance.getProcessDefinitionId());
 		Task task = taskService.createTaskQuery().taskCandidateUser("kermit").singleResult();
 		assertNotNull(task);
 		System.out.println("found task " + task.getName());
