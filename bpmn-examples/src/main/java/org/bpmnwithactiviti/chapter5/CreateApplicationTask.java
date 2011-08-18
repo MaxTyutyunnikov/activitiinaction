04dc1b35a068eaba7d8f61e9c4114f516fb56ee4
tijs.rademakers <tijs.rademakers@gmail.com>
2011-08-18 09:22:39 +0000

    Refactoring of chapter numbers

--- bpmn-examples/src/main/java/org/bpmnwithactiviti/chapter5/CreateApplicationTask.java
+++ bpmn-examples/src/main/java/org/bpmnwithactiviti/chapter5/CreateApplicationTask.java
@@ -1,9 +1,9 @@
-package org.bpmnwithactiviti.chapter4;
+package org.bpmnwithactiviti.chapter5;
 
 import org.activiti.engine.delegate.DelegateExecution;
 import org.activiti.engine.delegate.JavaDelegate;
 
-public class ApplicationCreator implements JavaDelegate {
+public class CreateApplicationTask implements JavaDelegate {
 
 	public void execute(DelegateExecution execution) {
 		LoanApplication la = new LoanApplication();
