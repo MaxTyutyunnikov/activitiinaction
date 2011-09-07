c934731b88302300e4c4939e953c6bfc5a0e9c9a
tijs.rademakers <tijs.rademakers@a29e19ff-66de-9391-0f37-b916ae5d8a96>
2011-09-07 19:43:11 +0000

    Mule changes for chapter 11

--- book-mule/src/test/java/org/bpmnwithactiviti/chapter9/mule/helloworld/MuleHelloTest.java
+++ book-mule/src/test/java/org/bpmnwithactiviti/chapter9/mule/helloworld/MuleHelloTest.java
@@ -7,11 +7,16 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.activiti.engine.RuntimeService;
-import org.activiti.engine.runtime.ProcessInstance;
 import org.bpmnwithactiviti.common.AbstractTest;
 import org.junit.Test;
+import org.mule.DefaultMuleMessage;
 import org.mule.api.MuleContext;
+import org.mule.api.MuleMessage;
+import org.mule.api.client.MuleClient;
+import org.mule.api.transport.PropertyScope;
+import org.mule.client.DefaultLocalMuleClient;
 import org.mule.context.DefaultMuleContextFactory;
+import org.mule.module.activiti.action.model.ProcessInstance;
 
 public class MuleHelloTest extends AbstractTest {
 
@@ -19,12 +24,17 @@ public class MuleHelloTest extends AbstractTest {
   public void testSend() throws Exception {
     MuleContext muleContext = new DefaultMuleContextFactory().createMuleContext("helloworld/application-context.xml");
     muleContext.start();
-    RuntimeService runtimeService = (RuntimeService) muleContext.getRegistry().get("runtimeService");
+    MuleClient muleClient = new DefaultLocalMuleClient(muleContext);
+    DefaultMuleMessage message = new DefaultMuleMessage("", muleContext);
     Map<String, Object> variableMap = new HashMap<String, Object>();
     variableMap.put("var1", "hello");
-    ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("helloWorldMule", variableMap);
+    variableMap.put("processDefinitionKey", "helloWorldMule");
+    message.setProperty("createProcessParameters", variableMap , PropertyScope.OUTBOUND);
+    MuleMessage responseMessage = muleClient.send("vm://create", message);
+    ProcessInstance processInstance = (ProcessInstance) responseMessage.getPayload();
     assertFalse(processInstance.isEnded());
-    Object result = runtimeService.getVariable(processInstance.getProcessInstanceId(), "var2");
+    RuntimeService runtimeService = (RuntimeService) muleContext.getRegistry().get("runtimeService");
+    Object result = runtimeService.getVariable(processInstance.getId(), "var2");
     assertEquals("world", result);
     muleContext.stop();
     muleContext.dispose();
