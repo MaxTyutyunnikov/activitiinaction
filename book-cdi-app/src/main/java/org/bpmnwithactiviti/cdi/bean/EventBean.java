d0a62c744518d4570a69e25049e6b1ccbfc7cea1
tijs.rademakers <tijs.rademakers@a29e19ff-66de-9391-0f37-b916ae5d8a96>
2011-09-21 20:55:43 +0000

    Chapter 9 codes

--- book-cdi-app/src/main/java/org/bpmnwithactiviti/cdi/bean/EventBean.java
+++ book-cdi-app/src/main/java/org/bpmnwithactiviti/cdi/bean/EventBean.java
@@ -1,16 +1,12 @@
 package org.bpmnwithactiviti.cdi.bean;
 
-import java.io.Serializable;
-
 import javax.enterprise.event.Observes;
 
 import org.activiti.cdi.BusinessProcessEvent;
 import org.activiti.cdi.BusinessProcessEventType;
 
-public class EventBean implements Serializable {
+public class EventBean {
 	
-  private static final long serialVersionUID = 1L;
-  
 	public void onProcessEvent(@Observes BusinessProcessEvent businessProcessEvent) {
     System.out.println("----- event type " + businessProcessEvent.getType().getTypeName() + " ------");
     System.out.println("Process instance id " + businessProcessEvent.getProcessInstanceId());
