427c88b3eb8d42222ded2cebfc56fb53b284c31a
andy.verberne <andy.verberne@gmail.com>
2011-12-04 12:10:01 +0000

    Added some comments to make the code easier to comprehend. Modified the code such that it only uses one task variable in stead of several. This prevents making mistakes. Notice that the old version had 3 cases where the 'task' variable was used whereas the 'queryTask' or 'claimedTask' variable should have been used! Added an extra test method to illustrate how a task can be created using a candidate group.

--- bpmn-examples/src/test/java/org/bpmnwithactiviti/chapter4/api/TaskServiceTest.java
+++ bpmn-examples/src/test/java/org/bpmnwithactiviti/chapter4/api/TaskServiceTest.java
@@ -5,11 +5,13 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.activiti.engine.IdentityService;
 import org.activiti.engine.RuntimeService;
 import org.activiti.engine.TaskService;
+import org.activiti.engine.identity.Group;
 import org.activiti.engine.identity.User;
 import org.activiti.engine.task.Task;
 import org.activiti.engine.test.ActivitiRule;
@@ -44,25 +46,66 @@ public class TaskServiceTest extends AbstractTest {
 	
 	@Test
 	public void createTask() {
+		// Create the user task
 		TaskService taskService = activitiRule.getTaskService();
 		Task task = taskService.newTask();
 		task.setName("Test task");
 		task.setPriority(100);
 		taskService.saveTask(task);
 		assertNull(task.getAssignee());
+		// Create the user
 		IdentityService identityService = activitiRule.getIdentityService();
 		User user = identityService.newUser("JohnDoe");
 		identityService.saveUser(user);
+		// Set the candidate user for the task
 		taskService.addCandidateUser(task.getId(), "JohnDoe");
-		Task queryTask = taskService.createTaskQuery().taskCandidateUser("JohnDoe").singleResult();
-		assertNotNull(queryTask);
-		assertEquals("Test task", queryTask.getName());
+		// Claim and complete the task
+		task = taskService.createTaskQuery().taskCandidateUser("JohnDoe").singleResult();
+		assertNotNull(task);
+		assertEquals("Test task", task.getName());
 		assertNull(task.getAssignee());
 		taskService.claim(task.getId(), "JohnDoe");
-		Task claimedTask = taskService.createTaskQuery().taskAssignee("JohnDoe").singleResult();
-		assertEquals("JohnDoe", claimedTask.getAssignee());
+		task = taskService.createTaskQuery().taskAssignee("JohnDoe").singleResult();
+		assertEquals("JohnDoe", task.getAssignee());
 		taskService.complete(task.getId());
-		Task completedTask = taskService.createTaskQuery().taskAssignee("JohnDoe").singleResult();
-		assertNull(completedTask);
+		task = taskService.createTaskQuery().taskAssignee("JohnDoe").singleResult();
+		assertNull(task);
 	}
+	
+	@Test
+	public void createTaskWithCandidateGroup() {
+		// Create the user task
+		TaskService taskService = activitiRule.getTaskService();
+		Task task = taskService.newTask();
+		task.setName("Test task");
+		task.setPriority(100);
+		taskService.saveTask(task);
+		assertNull(task.getAssignee());
+		// Create users and group
+		IdentityService identityService = activitiRule.getIdentityService();
+		User user = identityService.newUser("JohnDoe1");
+		identityService.saveUser(user);
+		user = identityService.newUser("JohnDoe2");
+		identityService.saveUser(user);
+		Group group  = identityService.newGroup("JohnDoes");
+		identityService.saveGroup(group);
+		identityService.createMembership("JohnDoe1", group.getId());
+		identityService.createMembership("JohnDoe2", group.getId());
+		// Set the candidate group for the task
+		taskService.addCandidateGroup(task.getId(), group.getId());
+		// Claim and complete the task
+		task = taskService.createTaskQuery().taskCandidateUser("JohnDoe1").singleResult();
+		assertNotNull(task);
+		assertEquals("Test task", task.getName());
+		assertNull(task.getAssignee());
+		taskService.claim(task.getId(), "JohnDoe1");
+		task = taskService.createTaskQuery().taskCandidateUser("JohnDoe2").singleResult();
+		assertNull(task);
+		task = taskService.createTaskQuery().taskAssignee("JohnDoe1").singleResult();
+		assertEquals("JohnDoe1", task.getAssignee());
+		taskService.complete(task.getId());
+		task = taskService.createTaskQuery().taskAssignee("JohnDoe1").singleResult();
+		assertNull(task);
+	}
+
 }
