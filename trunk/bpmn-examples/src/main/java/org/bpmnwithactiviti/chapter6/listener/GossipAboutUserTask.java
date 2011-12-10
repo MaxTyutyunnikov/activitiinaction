eb5b71907512eeb18195beed143b88e74231a0b6
andy.verberne@gmail.com <andy.verberne@gmail.com@a29e19ff-66de-9391-0f37-b916ae5d8a96>
2011-12-10 11:21:24 +0000

    Moved the println statement above the reassignment of the user task such that the console output reflects the real order in which the events are fired.

--- trunk/bpmn-examples/src/main/java/org/bpmnwithactiviti/chapter6/listener/GossipAboutUserTask.java
+++ trunk/bpmn-examples/src/main/java/org/bpmnwithactiviti/chapter6/listener/GossipAboutUserTask.java
@@ -4,12 +4,12 @@ import org.activiti.engine.delegate.DelegateTask;
 import org.activiti.engine.delegate.TaskListener;
 
 public class GossipAboutUserTask {
-	
+
 	public void gossipTask(DelegateTask task, String eventName) {
-		if(TaskListener.EVENTNAME_CREATE.equals(eventName)) {
-			task.setAssignee("John");
+		if (TaskListener.EVENTNAME_CREATE.equals(eventName)) {
 			System.out.println("Drink user task is created and assigned to John");
-		} else if(TaskListener.EVENTNAME_ASSIGNMENT.equals(eventName)) {
+			task.setAssignee("John");
+		} else if (TaskListener.EVENTNAME_ASSIGNMENT.equals(eventName)) {
 			System.out.println("Drink user task is assigned to " + task.getAssignee());
 		}
 		EventUtil.addEvent(task, eventName);
