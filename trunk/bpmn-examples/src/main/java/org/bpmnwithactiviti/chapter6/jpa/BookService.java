114c573b6b6d0e301593b817bf446bed1cda6e78
andy.verberne@gmail.com <andy.verberne@gmail.com@a29e19ff-66de-9391-0f37-b916ae5d8a96>
2011-12-10 06:40:44 +0000

    Just reformatted the code.

--- trunk/bpmn-examples/src/main/java/org/bpmnwithactiviti/chapter6/jpa/BookService.java
+++ trunk/bpmn-examples/src/main/java/org/bpmnwithactiviti/chapter6/jpa/BookService.java
@@ -6,15 +6,15 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 public class BookService {
-	
+
 	@PersistenceContext
 	private EntityManager entityManager;
-	
+
 	public Book createBook(List<String> authorList) {
 		Book book = new Book();
 		for (String author : authorList) {
-      book.getAuthors().add(author);
-    }
+			book.getAuthors().add(author);
+		}
 		entityManager.persist(book);
 		return book;
 	}
