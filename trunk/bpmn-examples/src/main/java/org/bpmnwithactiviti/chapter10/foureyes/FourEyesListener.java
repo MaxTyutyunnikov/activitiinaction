e28bca9660a9362b7f5b7166cb9a950623e6140b
andy.verberne@gmail.com <andy.verberne@gmail.com@a29e19ff-66de-9391-0f37-b916ae5d8a96>
2011-12-19 20:04:44 +0000

    Using the StringUtil class from drools in a class which is supposed to be reusable is not a good idea.

--- trunk/bpmn-examples/src/main/java/org/bpmnwithactiviti/chapter10/foureyes/FourEyesListener.java
+++ trunk/bpmn-examples/src/main/java/org/bpmnwithactiviti/chapter10/foureyes/FourEyesListener.java
@@ -8,45 +8,42 @@ import org.activiti.engine.delegate.DelegateTask;
 import org.activiti.engine.delegate.TaskListener;
 import org.activiti.engine.history.HistoricTaskInstance;
 import org.activiti.engine.impl.el.FixedValue;
-import org.drools.core.util.StringUtils;
 
 public class FourEyesListener implements TaskListener {
-	
+
 	private FixedValue otherTaskId;
 	private FixedValue processEngineName;
 
 	@Override
-  public void notify(DelegateTask delegateTask) {
+	public void notify(DelegateTask delegateTask) {
 		String name = null;
-		if(processEngineName != null && StringUtils.isEmpty(processEngineName.getExpressionText()) == false) {
+		if (processEngineName != null) {
 			name = processEngineName.getExpressionText();
 		} else {
 			name = ProcessEngines.NAME_DEFAULT;
 		}
 		ProcessEngine processEngine = ProcessEngines.getProcessEngines().get(name);
-	  HistoryService historyService = processEngine.getHistoryService();
-	  HistoricTaskInstance historicTask = historyService.createHistoricTaskInstanceQuery()
-	  		.processInstanceId(delegateTask.getProcessInstanceId())
-	  		.taskDefinitionKey(otherTaskId.getExpressionText())
-	  		.singleResult();
-	  
-	  if(historicTask == null) {
-	  	throw new ActivitiException("The previous task " + otherTaskId.getExpressionText() + " could not be found");
-	  }
-	  
-	  String claimer = delegateTask.getAssignee();
-	  String previousAssigneee = historicTask.getAssignee();
-	  
-	  if(claimer.equalsIgnoreCase(previousAssigneee)) {
-	  	throw new ActivitiException("Assignee of task " + otherTaskId.getExpressionText() + " is not allowed to claim this task");
-	  }
-  }
+		HistoryService historyService = processEngine.getHistoryService();
+		HistoricTaskInstance historicTask = historyService.createHistoricTaskInstanceQuery().processInstanceId(delegateTask.getProcessInstanceId())
+				.taskDefinitionKey(otherTaskId.getExpressionText()).singleResult();
+
+		if (historicTask == null) {
+			throw new ActivitiException("The previous task " + otherTaskId.getExpressionText() + " could not be found");
+		}
+
+		String claimer = delegateTask.getAssignee();
+		String previousAssigneee = historicTask.getAssignee();
+
+		if (claimer.equalsIgnoreCase(previousAssigneee)) {
+			throw new ActivitiException("Assignee of task " + otherTaskId.getExpressionText() + " is not allowed to claim this task");
+		}
+	}
 
 	public void setOtherTaskId(FixedValue otherTaskId) {
-  	this.otherTaskId = otherTaskId;
-  }
+		this.otherTaskId = otherTaskId;
+	}
 
 	public void setProcessEngineName(FixedValue processEngineName) {
-  	this.processEngineName = processEngineName;
-  }
+		this.processEngineName = processEngineName;
+	}
 }
