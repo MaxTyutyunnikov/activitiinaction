ebcc8a4eceb05c2c3a92856142d1e688467dfcbe
tijs.rademakers <tijs.rademakers@a29e19ff-66de-9391-0f37-b916ae5d8a96>
2012-03-17 15:10:17 +0000

    Refactored example to new camunda fox engine

--- trunk/book-jee6/book-jee6-process/src/main/java/org/bpmnwithactiviti/jee6/task/HelloDelegate.java
+++ trunk/book-jee6/book-jee6-process/src/main/java/org/bpmnwithactiviti/jee6/task/HelloDelegate.java
@@ -3,18 +3,16 @@ package org.bpmnwithactiviti.jee6.task;
 import javax.ejb.EJB;
 import javax.inject.Named;
 
-import org.activiti.engine.delegate.DelegateExecution;
-import org.activiti.engine.delegate.JavaDelegate;
+import org.bpmnwithactiviti.jee6.Hello;
 
 @Named
-public class HelloDelegate implements JavaDelegate {
+public class HelloDelegate {
 
 	@EJB(lookup="java:global/book-jee6-ejb/HelloBean")
 	private Hello helloBean;
 	
-	@Override
-  public void execute(DelegateExecution execution) throws Exception {
-		helloBean.sayHello((String) execution.getVariable("name"));
+  public void sayHello(String name) {
+		helloBean.sayHello(name);
   }
 
 }
